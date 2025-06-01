91ab2496d40bea99ea2427c34d5cf14566327fa0
Ramy Kaddouri <myramkadd@gmail.com>
2025-06-01 01:19:48 -0400

    fix icons

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -6,7 +6,6 @@ return {
   version = '*',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
   },
   lazy = false,
@@ -22,6 +21,37 @@ return {
           mappings = {
             ['\\'] = 'close_window',
           },
+        },
+      },
+      default_component_configs = {
+        icon = {
+          provider = function(icon, node) -- setup a custom icon provider
+            local text, hl
+            local mini_icons = require 'mini.icons'
+            if node.type == 'file' then -- if it's a file, set the text/hl
+              text, hl = mini_icons.get('file', node.name)
+            elseif node.type == 'directory' then -- get directory icons
+              text, hl = mini_icons.get('directory', node.name)
+              -- only set the icon text if it is not expanded
+              if node:is_expanded() then
+                text = nil
+              end
+            end
+
+            -- set the icon text/highlight only if it exists
+            if text then
+              icon.text = text
+            end
+            if hl then
+              icon.highlight = hl
+            end
+          end,
+        },
+        kind_icon = {
+          provider = function(icon, node)
+            local mini_icons = require 'mini.icons'
+            icon.text, icon.highlight = mini_icons.get('lsp', node.extra.kind.name)
+          end,
         },
       },
     }
